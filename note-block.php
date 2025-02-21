04f7a12908ce8d8a7b857856830eafd65884e6e4
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2025-02-21 16:44:37 +0100

    adding general info

--- note-block.php
+++ note-block.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Note Block
  * Description:       Example block scaffolded with Create Block tool.
- * Version:           1.1.3
+ * Version:           1.1.5
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            The WordPress Contributors
